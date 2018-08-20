c1565149ee480400a9d7c057e18ffde507c4c733
Skyler Lewis <skyler.lewis@canopytax.com>
2018-08-19 20:12:57 -0600

    update konami env

---
+++ assets/js/konami.js
@@ -0,0 +1,74 @@
+let startGame = false;
+let kStartButton = document.getElementById("konami-start");
+// kStartButton
+
+//Blinky text/terminal questions
+
+
+//
+// var canvas = document.getElementById("myCanvas");
+//
+// paper.install(window);
+// paper.setup(canvas);
+//
+// new Path.Circle({
+// 	center: [0, 0],
+// 	radius: 100,
+// 	fillColor: 'white',
+// 	strokeColor: 'black'
+// });
+//
+// var Eye = function(props) {
+// 	var args = arguments;
+//
+// 	this.center = args[0] instanceof Array ? args[0] : props.center || [20, 20];
+//   this.radius = args[1] || props.radius || 100;
+// 	this.irisRadius = args[2] || props.irisRadius || this.radius / 2.4;
+// 	this.zDistance = props.zDistance || 120;
+//
+// 	this.moveRatio = this.radius / (this.zDistance + this.radius);
+// 	this.maxIrisOffset = this.radius - this.irisRadius;
+//
+// 	this.sclera = new Path.Circle({
+//   	center: this.center,
+//     radius: this.radius,
+//     fillColor: 'white',
+//     strokeColor: 'black'
+//   });
+//
+// 	this.iris = new Path.Circle({
+//   	center: this.center,
+//     radius: this.irisRadius,
+//     fillColor: 'black'
+//   });
+//
+// 	this.trackingVector = new Path.Line(this.center);
+// }
+//
+// Eye.prototype.track = function(point) {
+// 	this.trackingVector.lastSegment.point = point;
+// 	this.iris.position = this.trackingVector.getPointAt(Math.min(
+//   	this.trackingVector.length * this.moveRatio,
+//     this.maxIrisOffset
+//   ));
+// }
+// var xMax = canvas.clientWidth;
+// var yMax = canvas.clientHeight;
+// var pupillaryDistance = 100;
+//
+// var leftEye = new Eye([xMax / 2 - pupillaryDistance / 2, yMax / 2], 50);
+// var rightEye = new Eye([xMax / 2 + pupillaryDistance / 2, yMax / 2], 35);
+//
+// view.onMouseMove = function(event) {
+// 	leftEye.track(event.point);
+//   rightEye.track(event.point);
+// };
+
+
+//hint about clicking the background 100 times
+//    A click for every year of the last century
+
+// A ruin is found with something interesting upon it.
+//    image of ruin, with binary code on it, which translates to a base64 hash, which is a key to the input field provided
+
+// You ha
